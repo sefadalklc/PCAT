670ecad03b43b0dc3a75e8997ac587b1f5f287fc
sefadalklc <sefadalk11@gmail.com>
2022-03-12 15:35:47 +0300

    Model ve dinamik içerikler oluşturuldu

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+const { default: mongoose } = require('mongoose');
 const app = express();
+
+//connect DB 
+mongoose.connect('mongodb://localhost/pcat-test-db',{
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
 
 
 //TEMPLATE ENGINE
@@ -14,8 +22,11 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 //ROUTES
-app.get('/', (req,res) => {
-   res.render('index')
+app.get('/', async (req,res) => {
+   const photos = await Photo.find({})
+   res.render('index',{
+      photos
+   });
 });
 app.get('/about', (req,res) => {
     res.render('about')
@@ -23,8 +34,8 @@ app.get('/about', (req,res) => {
  app.get('/add', (req,res) => {
     res.render('add')
  });
- app.post('/photos', (req,res) => {
-   console.log(req.body)
+ app.post('/photos', async (req,res) => {
+   await Photo.create(req.body)
    res.redirect('/')
 });
 
