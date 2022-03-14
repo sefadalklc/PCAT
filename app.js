60e9cbc523909aab9a881df5067b37e54f18ea3e
sefadalklc <sefadalk11@gmail.com>
2022-03-14 15:43:02 +0300

    upload image

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
+const fileUpload = require('express-fileupload');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
@@ -18,12 +20,13 @@ app.set("view engine", "ejs");
 
 //Middleware
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(fileUpload());
 
 //ROUTES
 app.get('/', async (req,res) => {
-   const photos = await Photo.find({})
+   const photos = await Photo.find({}).sort('-dateCreated');
    res.render('index',{
       photos
    });
@@ -44,12 +47,36 @@ app.get('/about', (req,res) => {
  app.get('/add', (req,res) => {
     res.render('add')
  });
+
+//////////////////////
+ 
  app.post('/photos', async (req,res) => {
-   await Photo.create(req.body)
-   res.redirect('/')
-});
+  
+  //console.log(req.files.image)
+  
+   // await Photo.create(req.body)
+   //res.redirect('/')
+ 
+const uploadDir = 'public/uploads';
+   if(!fs.existsSync(uploadDir)){
+      fs.mkdirSync(uploadDir)
+   }
 
+   let uploadeImage = req.files.image;
+   let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
 
+   uploadeImage.mv(uploadPath, 
+      async () => {
+         await Photo.create({
+            ...req.body,
+            image:'/uploads/' + uploadeImage.name
+         })
+         res.redirect('/');
+      })
+
+   });
+
+//////////////////////////////////
 const port = 3000;
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı..`);
