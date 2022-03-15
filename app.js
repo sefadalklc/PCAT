de422718c19f45016adf924b64a0aedbc32cf5c7
sefadalklc <sefadalk11@gmail.com>
2022-03-15 23:23:43 +0300

    delete photo

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 mongoose.connect('mongodb://localhost/pcat-test-db',{
    useNewUrlParser: true,
    useUnifiedTopology: true,
+   
 });
 
 
@@ -24,7 +25,9 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(fileUpload());
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method', {
+   methods:['POST','GET']
+}));
 
 //ROUTES
 app.get('/', async (req,res) => {
@@ -95,6 +98,14 @@ app.put('/photos/:id', async (req,res) => {
    res.redirect(`/photos/${req.params.id}`)
 
 });
+
+app.delete('/photos/:id', async (req,res) => {
+   const photo = await Photo.findOne({_id: req.params.id})
+   let deletedImage = __dirname + '/public' + photo.image;
+   fs.unlinkSync(deletedImage);
+   await Photo.findByIdAndRemove(req.params.id)
+   res.redirect('/')
+})
 
 const port = 3000;
 app.listen(port, () => {
