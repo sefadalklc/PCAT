16c2040024be78065653f6226d9a5d9209877af2
sefadalklc <sefadalk11@gmail.com>
2022-03-15 13:36:22 +0300

    update a photo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
+const methodOverride = require('method-override');
 const fs = require('fs');
 const Photo = require('./models/Photo');
 const fileUpload = require('express-fileupload');
@@ -23,6 +24,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 //ROUTES
 app.get('/', async (req,res) => {
@@ -77,6 +79,23 @@ const uploadDir = 'public/uploads';
    });
 
 //////////////////////////////////
+app.get('/photos/edit/:id', async (req,res) => {
+   const photo = await Photo.findOne({_id: req.params.id});
+   res.render('edit', {
+      photo,
+   });
+});
+
+app.put('/photos/:id', async (req,res) => {
+   const photo = await Photo.findOne({_id: req.params.id});
+   photo.title = req.body.title;
+   photo.description = req.body.description;
+   photo.save()
+
+   res.redirect(`/photos/${req.params.id}`)
+
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı..`);
